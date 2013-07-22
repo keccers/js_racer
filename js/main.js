3a96d749ca41e38ae65aeb917d3eb1f8a0f9c99b
dbc-apprentice <apprentice@devbootcamp.com>
2013-07-22 16:09:58 -0500

    Finished

--- js/main.js
+++ js/main.js
@@ -1,1 +1,23 @@
+$(document).ready(function() {
+
+  $('body').keydown(function(event) {
+  // first player movements
+    if(event.which == 65) {
+     //console.log($('#player1_strip td'));
+     $('#player1_strip td.active').removeClass('active').next().addClass('active');
+     //.next('td').addClass('active');
+     //alert('You hit A whatup'); 
+    }
+     //second player movements
+    if (event.which == 76) {
+      $('#player2_strip td.active').removeClass('active').next().addClass('active');
+    }
+    if ($('table.racer_table tr td:last-child').hasClass('active')) {
+      $('body').unbind();
+      $('table.racer_table tr td:last-child.active').css('background-image', 'url(file:///Users/apprentice/Dropbox/DBC-Chicago/Grasshoppers/rburbank/p2_week5/js-racer/img/winner.gif)');
+    }
+  });
+
+
+});
 
